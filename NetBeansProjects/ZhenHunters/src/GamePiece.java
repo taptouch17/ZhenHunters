3ae6ee2894ced110d5a875db0d65cddafbea457d
taptouch17 <31634464+taptouch17@users.noreply.github.com>
2018-02-28 21:04:27 -0400

    Initial Commit

--- NetBeansProjects/ZhenHunters/src/GamePiece.java
+++ NetBeansProjects/ZhenHunters/src/GamePiece.java
@@ -10,65 +10,110 @@
  */
 public class GamePiece {
     
-  private String type;
-  private char symbol;
-  private int rowPos;
-  private int colPos;
+  private String type; //String describing type of game piece.
+  private char symbol; //Char describing game piece symbol.
+  private int rowPos; // row position of piece.
+  private int colPos;// column position of piece.
   
-  // Constructor to initialize type and sumbol
 
     /**
-     *
+     * Constructor to initialise the fields of GamePiece.
      */
-  public GamePiece() {
+  public GamePiece() 
+  {
       type = "?";
       symbol = '?';
+      rowPos = 0;
+      colPos = 0;
   }
   
-  // Accessor to get Type
+   
 
     /**
-     *
-     * @return
+     * Accessor to get Type
+     * @return type
      */
-  public String getType() {
+  public String getType() 
+  {
       return type;
-  }
+  } //getType
   
-  // Mutator for user to set the Type
-  public void setType(String theType) {
+   
+
+    /**
+     * Mutator for user to set the Type
+     * @param theType The type of piece.
+     */
+  public void setType(String theType) 
+  {
       type = theType;
-  }
+  }//setType
   
- // Accessor to get Symbol 
- public char getSymbol() {
+ 
+
+    /**
+     * Accessor to get Symbol
+     * @return symbol
+     */
+ public char getSymbol() 
+ {
      return symbol;
- }
+ }//getSymbol
  
- // Mutator for user to set the symbol
- public void setSymbol(char theSymbol) {
+ 
+
+    /**
+     * Mutator for user to set the symbol
+     * @param theSymbol the symbol used to describe the GamePiece
+     */
+ public void setSymbol(char theSymbol) 
+ {
      symbol = theSymbol;
- }
+ }//setSymbol
  
- // Accessor to get the rowPos field
- public int getRowPos() {
+ 
+
+    /**
+     * Accessor to get the rowPos field
+     * @return rowPos
+     */
+ public int getRowPos()
+ {
      return rowPos;
- }
+ }//getRowPos
  
- // Mutator to allow user to set rowPos
- public void setRowPos(int row) {
+  
+
+    /**
+     * Mutator to allow user to set rowPos
+     * @param row the row position
+     */
+ public void setRowPos(int row) 
+ {
      rowPos = row;
- }
+ }//setRowPos
  
- // Accessor to get the ColPos field
- public int getColPos() {
+ 
+
+    /**
+     * Accessor to get the ColPos field
+     * @return colPos
+     */
+ public int getColPos() 
+ {
      return colPos;
- }
+ }//getColPos
     
- // Mutator to allow setting ColPos
- public void setColPos(int column) {
+ 
+
+    /**
+     * Mutator to allow setting ColPos
+     * @param column column of the piece.
+     */
+ public void setColPos(int column) 
+ {
      colPos = column;
- }   
+ }//setColPos   
  
  
-}
+}//GamePiece
